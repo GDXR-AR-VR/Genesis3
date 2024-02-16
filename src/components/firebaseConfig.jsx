fb57e0b015e17b9ec02c04f97ecd99cdaa274d69
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2024-02-16 09:32:10 +0530

    firebase setup

---
+++ src/components/firebaseConfig.jsx
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { setDoc, collection, doc } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "genesis-iii.firebaseapp.com",
+  projectId: "genesis-iii",
+  storageBucket: "genesis-iii.appspot.com",
+  messagingSenderId: "826112030743",
+  appId: "1:826112030743:web:60ab9ff12977bdf8934452",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const newRecord = async (info) => {
+  try {
+    const docRef = await setDoc(doc(db, "sponsorList", info.name), info);
+    return "success";
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    return "fail";
+  }
+};
+
+export default newRecord;
+// Initialize Firebase
--- src/pages/sponsorUs.jsx
+++ src/pages/sponsorUs.jsx
@@ -1,10 +1,53 @@
 import { motion } from "framer-motion";
 import sponsorUs from "../assets/sponsorUs.png";
+import { useEffect, useRef, useState } from "react";
+import newRecord from "../components/firebaseConfig";
 export default function SponsorUs() {
-  const submit = (e) => {
-    e.preventDefault();
-    console.log("submitted");
+  const [prompt, setPrompt] = useState("Submit");
+  const name = useRef(null);
+  const desc = useRef(null);
+  const phone = useRef(null);
+  const email = useRef(null);
+  const contribution = useRef(null);
+  const expectation = useRef(null);
+  const submit = async () => {
+    const sponsorInfo = {
+      name: name.current.value,
+      phone: phone.current.value,
+      email: email.current.value,
+      description: desc.current.value,
+      contribution: contribution.current.value,
+      expectation: expectation.current.value,
+    };
+    if (
+      sponsorInfo.name === "" ||
+      sponsorInfo.phone === "" ||
+      sponsorInfo.email === "" ||
+      sponsorInfo.contribution === "" ||
+      sponsorInfo.expectation === ""
+    ) {
+      setPrompt("Try Again");
+      return;
+    }
+    const response = await newRecord(sponsorInfo);
+    if (response === "success") {
+      setPrompt("Success");
+      name.current.value = "";
+      email.current.value = "";
+      phone.current.value = "";
+      email.current.value = "";
+      contribution.current.value = "";
+      expectation.current.value = "";
+    } else {
+      setPrompt("Try again");
+    }
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPrompt("Submit");
+    }, 4000);
+  }, [prompt]);
   return (
     <>
       <div className="sponsors relative flex-col h-screen w-screen bg-[#ECE8E1] font-[BerlinSans] overflow-hidden  ">
@@ -19,47 +62,69 @@ export default function SponsorUs() {
         <div className=" glass rounded-3xl border-[1px] p-4 justify-center select-none items-center align-middle m-auto w-9/12 md:w-[400px] z-20 flex-col gap-4">
           <p className="text-white">Name of Organisation</p>
           <input
-            // name="name "
+            required={true}
+            ref={name}
             type="text"
+            className="h-8 rounded-xl w-full z-20 p-2"
+          ></input>
+          <p className="text-white">Phone no.</p>
+          <input
+            required={true}
+            ref={phone}
+            type="number"
             className="h-8 rounded-xl w-full z-20 p-2"
           ></input>
 
           <p className="text-white">E-mail</p>
           <input
+            required={true}
+            ref={email}
             name="email"
             type="email"
             className="h-8 w-full rounded-xl p-2"
           ></input>
           <p className="text-white">Description</p>
           <input
+            ref={desc}
             name="desc"
             type="text"
             className="h-8 w-full rounded-xl p-2"
           ></input>
           <p className="text-white">How do you want to contribute</p>
           <textarea
+            required={true}
+            ref={contribution}
             name="contribution"
             type="text"
             className="h-12 w-full rounded-xl p-2"
           ></textarea>
           <p className="text-white">What do you expect from us</p>
           <textarea
+            required={true}
+            ref={expectation}
             name="expectation"
             type="text"
             className="h-12 w-full rounded-xl p-2"
           ></textarea>
           <br />
           <div className="w-full md:mt-10 justify-center">
-            <motion.div
-              whileHover={{ scale: 1.1 }}
-              type="submit"
-              onClick={() => {
-                submit(e);
-              }}
-              className="w-fit text-2xl p-3 font-bold font-[BerlinSans] select-none h-12 text-white text-center m-auto register bg-red-500 "
-            >
-              Submit
-            </motion.div>
+            <div className="m-auto w-fit">
+              <motion.button
+                onClick={() => {
+                  submit();
+                }}
+                whileHover={{ scale: 1.1 }}
+                className={`w-fit text-2xl p-3 font-bold font-[BerlinSans] select-none h-12 text-white text-center m-auto register ${
+                  prompt === "Submit"
+                    ? "bg-red-500"
+                    : prompt === "Success"
+                    ? "bg-green-500"
+                    : "bg-black"
+                }`}
+              >
+                {prompt}
+              </motion.button>
+            </div>
           </div>
         </div>
       </div>
